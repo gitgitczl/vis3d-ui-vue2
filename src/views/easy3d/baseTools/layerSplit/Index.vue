21ee9217f48527650e010e4267ede3164392192e
gitgitczl <819576814@qq.com>
2022-08-14 23:23:43 +0800

    修改分屏对比

--- src/views/easy3d/baseTools/layerSplit/Index.vue
+++ src/views/easy3d/baseTools/layerSplit/Index.vue
@@ -10,7 +10,7 @@
 import Card from "@/views/easy3d/components/card/Card.vue";
 import LayerSplit from "@/lib/easy3d/layerSplit/layerSplit.js";
 let layerSplit = null;
-let nowLayerAttr = {};
+let lastLayerAttr = {}; // 当前操作的图层
 export default {
   name: "layerSplit",
   components: {
@@ -29,18 +29,21 @@ export default {
   },
 
   mounted() {
-    // 默认选中第一个展示
+
     let operateLayers = this.$store.state.map3d.operateLayers;
     let res = this.getAllLayers(operateLayers);
 
     let allLayers = res.layers;
     for (let i = 0; i < allLayers.length; i++) {
       let item = allLayers[i];
-      if (item.layerSplit == true) {
+      item = JSON.parse(JSON.stringify(item));
+      item.initState = item.show; // 记录原始状态
+      if (item.layerSplit == true) { // 是否用来做分屏对比
         this.layerList.push(item);
       }
     }
 
+    // 默认选中第一个展示
     this.nowSelectId = this.layerList[0].id;
 
     let lyrObj = window.mapViewer.operateLayerTool.getLayerObjById(
@@ -48,19 +51,43 @@ export default {
     ).layerObj;
     lyrObj.setVisible(true);
 
-    if (!layerSplit)
+    lastLayerAttr = this.layerList[0];
+
+    if (!layerSplit) {
       layerSplit = new LayerSplit(window.viewer, {
         layer: lyrObj._layer,
       });
+    }
+
   },
   destroyed() {
-    this.resetLastlyr();
     if (layerSplit) {
       layerSplit.destroy();
       layerSplit = null;
     }
+    this.resetLastlyr();
   },
   methods: {
+    changeLayer(attr) {
+      // 还原上一个选中的对象
+      this.resetLastlyr();
+      let lyr = this.getLayerObjById(attr.id);
+      if (!lyr || !layerSplit) return;
+      lastLayerAttr = attr;
+    },
+    // 还原上一个操作
+    resetLastlyr() {
+      if (!lastLayerAttr) return;
+      let initState = lastLayerAttr.initState;
+      let lyrObj = window.mapViewer.operateLayerTool.getLayerObjById(
+        lastLayerAttr.id
+      ).layerObj;
+      if (lyrObj) lyrObj.setVisible(initState);
+      lastLayerAttr = null;
+    },
+    close() {
+      this.$emit("close", "layerSplit");
+    },
     // childeren转为线性
     getAllLayers(lys) {
       lys = lys || {};
@@ -89,33 +116,7 @@ export default {
         layers,
         groups,
       };
-    },
-    changeLayer(attr) {
-      let lyr = this.getLayerObjById(attr.id);
-      if (!lyr) return;
-      if (layerSplit) layerSplit.setLayer(lyr);
-      // 还原上一个选中的对象
-      this.resetLastlyr();
-      nowLayerAttr = {
-        oldVisible: lyr._layer.show,
-      };
-      nowLayerAttr = JSON.parse(JSON.stringify(nowLayerAttr));
-      nowLayerAttr.layerObj = lyr;
-      nowLayerAttr.layer = lyr._layer;
-      nowLayerAttr.newVisible = true;
-      // 控制对应图层的显示隐藏
-      this.$store.commit("setOperateLayerVisible", {
-        attr: JSON.parse(JSON.stringify(attr)),
-        visible: true,
-      });
-    },
-    resetLastlyr(){
-      
-    },
-    close() {
-      this.$emit("close", "layerSplit");
-    },
-   
+    }
   },
 };
 </script>
