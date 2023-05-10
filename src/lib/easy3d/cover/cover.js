b9db55b45ff8452218a3c0026d03186dc6aadd4a
gitgitczl <819576814@qq.com>
2023-05-10 16:35:39 +0800

    修改引入方式

--- src/lib/easy3d/cover/cover.js
+++
@@ -1,8 +0,0 @@
-// import echartsLayer from "./echartsLayer";
-// import mapvLayer from "./mapvLayer";
-import Heatmap from "./heatmap";
-import Heatmap3d from "./heatmap3d";
-export default {
-    // echartsLayer, mapvLayer,
-   Heatmap, Heatmap3d
-}
--- src/lib/easy3d/gadgets/gadgets.js
+++
@@ -1,8 +0,0 @@
-// 相关小工具
-import GaodeRoute from "./GaodeRoute";
-import viewToPoint from "./viewToPoint";
-import viewAround from "./viewAround";
-import viewControl from "./viewControl";
-export default {
-    GaodeRoute, viewToPoint, viewAround, viewControl
-}
