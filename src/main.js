f7c60b582e632fad904eb9a473eedbf2cd7be440
gitgitczl <819576814@qq.com>
2023-08-03 16:36:23 +0800

    修改类库

--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
 /* import less from 'less' */
-import easy3d from "@/lib/easy3d-resource/easy3d.export.js"
+// import easy3d from "@/lib/easy3d-resource/easy3d.export.js"
 // ============= 加密后 =================
-/* import "@/lib/easy3d/easy3d.min.css"
-import easy3d from "@/lib/easy3d/easy3d.min.js" */
+import "@/lib/easy3d/easy3d.min.css"
+import easy3d from "@/lib/easy3d/easy3d.min.js"
 
 import '@/assets/font/iconfont.css'
 // 本地加密类库
@@ -29,14 +29,13 @@ Vue.use(Print);
 import Card from "@/views/easy3d/components/card/Card.vue";
 Vue.component("Card", Card);
 
-
-let cesium = require('cesium/Cesium.js');
+/* let cesium = require('cesium/Cesium.js');
 var widgets = require('cesium/Widgets/widgets.css');
-
-Vue.prototype.Cesium = window.Cesium = cesium;
-
-Vue.prototype.easy3d = window.easy3d = easy3d;
 Vue.prototype.widgets = widgets
+Vue.prototype.Cesium = window.Cesium = cesium; */
+
+Vue.prototype.easy3d = window.easy3d = easy3d;  
+
 
 Vue.config.productionTip = false
 
