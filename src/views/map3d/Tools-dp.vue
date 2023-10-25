9d1475c66b0398e6d45d251670cdcdc4959e07a7
gitgitczl <819576814@qq.com>
2023-10-25 16:33:25 +0800

    修改相关

--- src/views/map3d/Tools-dp.vue
+++ src/views/map3d/Tools-dp.vue
@@ -1,53 +1,56 @@
 <template>
-    <ul class="tool-box">
-        <li>
-            <span class="iconfont icon-ditufuwu tool-icon"></span>
-            <p>底图</p>
-        </li>
-        <li>
-            <span class="iconfont icon-cengshu tool-icon"></span>
-            <p>图层</p>
-        </li>
-        <li class="tool-slide-down">
-            <el-dropdown>
-                <span class="el-dropdown-link tool-dropdown">
-                    <span class="iconfont icon-ditufuwu tool-icon"></span>
-                    <p>工具</p>
-                    <i class="el-icon-arrow-down el-icon--right"></i>
-                </span>
-                <el-dropdown-menu class="tool-dp-menu" slot="dropdown">
-                    <el-dropdown-item v-for="(item, index) in mapOperate" :key="index">
-                        <div class="tool-item">
-                            <span :class="['iconfont', item.icon]"></span>
-                            <p>{{ item.name }}</p>
-                        </div>
-                    </el-dropdown-item>
-                </el-dropdown-menu>
-            </el-dropdown>
-        </li>
-    </ul>
+    <div>
+        <!-- 地图侧边工具栏按钮 -->
+        <ul class="tool-box" v-show="isshowPanel">
+            <li @click="open({ toolName: 'baseMap' })">
+                <span class="iconfont icon-ditufuwu tool-icon"></span>
+                <p>底图</p>
+            </li>
+            <li @click="open({ toolName: 'layers' })">
+                <span class="iconfont icon-cengshu tool-icon"></span>
+                <p>图层</p>
+            </li>
+            <li class="tool-slide-down">
+                <el-dropdown>
+                    <span class="el-dropdown-link tool-dropdown">
+                        <span class="iconfont icon-gongjulan tool-icon"></span>
+                        <p>工具</p>
+                        <i class="el-icon-arrow-down el-icon--right"></i>
+                    </span>
+                    <el-dropdown-menu class="tool-dp-menu" slot="dropdown">
+                        <el-dropdown-item v-for="(item, index) in mapOperate" :key="index">
+                            <div class="tool-item" @click="open(item)">
+                                <span :class="['iconfont', item.icon]"></span>
+                                <p>{{ item.name }}</p>
+                            </div>
+                        </el-dropdown-item>
+                    </el-dropdown-menu>
+                </el-dropdown>
+            </li>
+        </ul>
+        <!-- 动态创建地图组件 -->
+        <div v-for="(item, index) in mapComphonets" :key="index">
+            <component :ref="item.toolName" :is="item.module" v-if="item.show" v-show="item.domShow" :title="item.name"
+                @fire="fire" :position="item.position" :size="item.size" :attr="item.attr" :iconfont="item.iconfont"
+                @close="close(item)">
+            </component>
+        </div>
+
+    </div>
 </template>
 <script>
 /* 工具栏风格二 下拉菜单效果 */
 import { workConfig } from "../map3d/config/export"
 /* 模块控制器 */
 import workControl from "./workControl.js";
+window.workControl = workControl; // 绑定到全局
+
 export default {
     name: "tools-dp",
     data() {
         return {
-            baseMap: {
-                icon: "icon-ditufuwu",
-                type: "baseMap",
-                name: "底图",
-                toolName: "baseMap",
-            },
-            layers: {
-                icon: "icon-cengshu",
-                type: "layers",
-                name: "图层",
-                toolName: "layers",
-            },
+            isshowPanel: true, // 是否显示操作按钮 
+            mapComphonets: [],
             mapOperate: [
                 {
                     icon: "icon-tushangcehui",
@@ -128,10 +131,78 @@ export default {
         }
     },
     mounted() {
-
+        // 初始化各工具组件
+        workControl.init(workConfig, (list) => {
+            this.mapComphonets = list;
+        });
     },
     methods: {
+        // 打开具体工具模块
+        open(item) {
+            if (item.toolName) {
+                workControl.openToolByName(item.toolName)
+            }
 
+            if (item.type == "scaleBig") { // 放大
+                if (!zoomTool) zoomTool = new this.vis3d.gadgets.ZoomTool(window.viewer);
+                zoomTool.forward();
+            }
+
+            if (item.type == "scaleSmall") { // 缩小
+                if (!zoomTool) zoomTool = new this.vis3d.gadgets.ZoomTool(window.viewer);
+                zoomTool.backward();
+            }
+
+            if (item.type == "update") { // 页面刷新
+                window.location.reload();
+            }
+
+            if (item.type === "fullScreen") {  // 全屏
+                this.screen();
+            }
+
+            if (item.type === "overviewMap") { // 鹰眼图
+                this.isOpenOverviewMap = !this.isOpenOverviewMap;
+                if (this.isOpenOverviewMap && !overviewMap) {
+                    overviewMap = new this.vis3d.common.OverviewMap(window.viewer);
+                } else {
+                    overviewMap.destroy();
+                    overviewMap = undefined;
+                }
+            }
+
+            if (item.type === "print") { // 地图打印
+                this.printMap();
+            }
+        },
+        close(item) {
+            workControl.closeToolByName(item.toolName);
+        },
+
+        // 地图打印
+        printMap() {
+            window.viewer.scene.render();
+            let container = document.getElementById(window.viewer._container.id);
+            html2canvas(container, {
+                backgroundColor: null,
+                useCORS: true,
+                windowHeight: document.body.scrollHeight,
+            }).then((canvas) => {
+                const url = canvas.toDataURL();
+                printJS({
+                    printable: url,
+                    type: "image",
+                    documentTitle: "地图输出",
+                });
+            });
+        },
+
+        // 触发组件的方法
+        fire(opt) {
+            let { toolName, methond, arg } = opt
+            if (!this.$refs[toolName] || !this.$refs[toolName][0]) return;
+            this.$refs[toolName][0][methond](arg);
+        }
     }
 }
 </script>
@@ -167,7 +238,7 @@ export default {
         }
 
         &:hover {
-          /*   background: var(--toolsMouseoverColor); */
+            /*   background: var(--toolsMouseoverColor); */
         }
 
         .tool-icon {
